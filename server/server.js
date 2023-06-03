dfaf4fa5e0799cb90d6df3bcac9e1ca1f08501f7
HISHAN03 <moidinhishan@gmail.com>
2023-06-03 11:42:49 +0530

    ninth commit

--- server/server.js
+++ server/server.js
@@ -19,9 +19,12 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
 //app.use(cors({ credentials: true, origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+
+
+
 const corsOptions = {
-  origin: ["https://chat-app-hishan03.vercel.app"],
   credentials: true,
+  origin: ["https://chat-app-git-main-hishan03.vercel.app/", "http://localhost:5173"],
 };
 
 app.use(cors(corsOptions));
